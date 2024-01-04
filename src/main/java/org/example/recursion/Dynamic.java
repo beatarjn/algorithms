5764299bf9ddfb7a54da779a746dee9d63b4cb18
berejman <beata.rejman@ergo.com>
2024-01-04 09:10:51 +0100

    Dynamic + memoization solution added

--- src/main/java/org/example/recursion/Dynamic.java
+++ src/main/java/org/example/recursion/Dynamic.java
@@ -15,4 +15,46 @@ public class Dynamic {
         int first = findMinRecursive(board, n - 1) + board[n];
         return Math.min(first, second);
     }
+
+    public int findMinMemo(int[] board, int n) {
+
+        int[] result = new int[n];
+        result[0] = board[0];
+        result[1] = board[1];
+        for (int i = 2; i < result.length; i++) {
+            result[i] = -1;
+        }
+        return memo(board, n, result);
+    }
+
+    private int memo(int[] board, int n, int[] result) {
+        int second;
+
+        if (n == 0) {
+            return result[n];
+        }
+        if (n != 1) {
+            int val;
+            if (result[n - 2] != -1) {
+                val = result[n - 2];
+            } else {
+                val = memo(board, n - 2, result);
+            }
+            second = val + board[n];
+        } else {
+            second = Integer.MAX_VALUE;
+        }
+        int first;
+        int val1;
+        if (result[n - 1] != -1) {
+            val1 = result[n - 1];
+        } else {
+            val1 = memo(board, n - 1, result);
+        }
+        first = val1 + board[n];
+
+        result[n] = Math.min(first, second);
+
+        return Math.min(first, second);
+    }
 }
--- src/test/java/org/example/recursion/DynamicTest.java
+++ src/test/java/org/example/recursion/DynamicTest.java
@@ -8,41 +8,52 @@ class DynamicTest {
     private Dynamic dynamic = new Dynamic();
 
     @Test
-    public void shouldReturn15Recursive() {
+    void shouldReturn15Recursive() {
         //given
         int[] stairs = new int[]{10, 15, 20};
 
         //when
-        int result = dynamic.findMinRecursive(stairs, stairs.length-1);
+        int result = dynamic.findMinRecursive(stairs, stairs.length - 1);
 
         //then
         Assertions.assertEquals(30, result);
     }
 
     @Test
-    public void shouldReturn6Recursive() {
+    void shouldReturn6Recursive() {
         //given
-        int[] stairs = new int[]{1,100,1,1,1,100,1,1,100,1};
+        int[] stairs = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
 
         //when
-        int result = dynamic.findMinRecursive(stairs, stairs.length-1);
+        int result = dynamic.findMinRecursive(stairs, stairs.length - 1);
 
         //then
         Assertions.assertEquals(6, result);
     }
 
     @Test
-    public void shouldReturn16Recursive() {
+    void shouldReturn16Recursive() {
         //given
-        int[] stairs = new int[]{1,4,5,2,3,9};
+        int[] stairs = new int[]{1, 4, 5, 2, 3, 9};
 
         //when
-        int result = dynamic.findMinRecursive(stairs, stairs.length-1);
+        int result = dynamic.findMinRecursive(stairs, stairs.length - 1);
 
         //then
         Assertions.assertEquals(16, result);
     }
 
+    @Test
+    void shouldReturn16Memo() {
+        //given
+        int[] stairs = new int[]{1, 4, 5, 2, 3, 9};
+
+        //when
+        int result = dynamic.findMinMemo(stairs, stairs.length - 1);
+
+        //then
+        Assertions.assertEquals(16, result);
+    }
 
 
 }
